e83a25139646298effcdabe3ced736e9311c581f
Caroline Frasca <103123936+sfc-gh-cfrasca@users.noreply.github.com>
2024-07-01 11:29:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.button("Click me!")
-st.text("Audio should autoplay after you click the button")
+st.write("Audio should autoplay after you click the button")
 st.caption("Since you need to click somewhere on the page before the browser will auto-play the audio clip")
 st.audio('timer_sound.mp3', format="audio/mpeg", autoplay=True)
